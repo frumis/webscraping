bf918a2a25bd8af190d0a577b8f9fc2102000f04
frumis <surfershar@gmail.com>
2025-08-19 18:14:24 +0200

    start

---
+++ modul-3.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.python.org/"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "lxml")
+
+# znajdź wszystkie linki w nagłówkach h2
+headings = soup.select("h2")
+
+for h in headings:
+    print("Nagłówek:", h.get_text(strip=True))
